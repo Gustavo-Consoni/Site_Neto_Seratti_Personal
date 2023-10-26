db6bb7ba1b6268fc44d2edf827016c9c0c7fde76
Gustavo Consoni <73336790+Gustavo-Consoni@users.noreply.github.com>
2023-10-25 23:22:58 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'mobile': "url('/arquivos/imagens/capa_mobile.jpg')",
-        'desktop': "url('/arquivos/imagens/capa_desktop.jpg')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
