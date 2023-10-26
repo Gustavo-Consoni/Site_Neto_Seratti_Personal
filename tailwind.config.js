3b2647bad40203e8ec35f53920b463c394af94f7
Gustavo Consoni <73336790+Gustavo-Consoni@users.noreply.github.com>
2023-10-26 04:02:16 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'mobile': "url('https://gustavo-consoni.github.io/Site_Neto_Seratti_Personal/arquivos/imagens/capa_mobile.jpg')",
-        'desktop': "url('https://gustavo-consoni.github.io/Site_Neto_Seratti_Personal/arquivos/imagens/capa_desktop.jpg')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
